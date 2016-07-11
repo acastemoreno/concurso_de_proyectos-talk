e7ac44d9935df8c6c1e9c4f115268d269353c614
acastemoreno <acastemoreno@gmail.com>
2016-07-11 06:59:13 -0500

    changes

--- slide_config.js
+++ slide_config.js
@@ -1,26 +1,26 @@
 var SLIDE_CONFIG = {
    // Slide Settings
    settings: {
-     title: 'Mejoramiento Informático en el Sistema de Matrícula', 
-     subtitle: 'Docente: Ing. ENRIQUE SARMIENTO SARMIENTO', 
-     useBuilds: true, // Default: true. False will turn off slide animation builds. 
-     usePrettify: true, // Default: true 
+     title: 'Estudio Técnico para la Implementación y Mejoramiento Informático del Sistema de Matrícula en la Facultad de Ingeniería Mecánica',
+     subtitle: 'Docente: Ing. Enrique Sarmiento Sarmiento',
+     useBuilds: true, // Default: true. False will turn off slide animation builds.
+     usePrettify: true, // Default: true
      enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
      enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-     favIcon: 'images/uni.png', 
+     favIcon: 'images/uni.png',
      fonts: [
        'Open Sans:regular,semibold,italic,italicsemibold',
        'Source Code Pro'
      ],
-   }, 
- 
+   },
+
    // Author information
    presenters: [{
-     name: 'Alumnos: Albert Castellano Moreno, Gabriela Alatrista Solís', 
-     company: '', 
-     gplus: '', 
-     twitter: '', 
-     www: '', 
-     github: '', 
+     name: 'Alumnos: Albert Castellano Moreno, Gabriela Alatrista Solís',
+     company: '',
+     gplus: '',
+     twitter: '',
+     www: '',
+     github: '',
    }]
 };
